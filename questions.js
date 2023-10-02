7855c99ac017e5d7c529fede7296a8f36c9c71a0
Chaithanya Yadav <131748804+chaithanya-9@users.noreply.github.com>
2023-10-02 17:13:15 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,112 @@
-
+let questions = [
+    {
+        numb: 1,
+        question: "What doeas HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What do you understand by HTML?",
+        answer: "D. All of the above",
+        options: [
+            "A. HTML describes the structure of a webpage",
+            "B. HTML is the standard markup language mainly used to create web pages",
+            "C. HTML consists of a set of elements that helps the browser how to view the content",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who is the father of HTML?",
+        answer: "B. Tim Berners-Lee",
+        options: [
+            "A. Rasmus Lerdorf",
+            "B. Tim Berners-Lee",
+            "C. Brendan Eich",
+            "D. Sergey Brin"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which is used to read an HTML page and render it?",
+        answer: "C. Web browser",
+        options: [
+            "A. Web server",
+            "B. Web network",
+            "C. Web browser",
+            "D. Web matrix"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which tag is used for inserting the largest heading in HTML?",
+        answer: "B. h1",
+        options: [
+            " A. head",
+            "B. h1",
+            "C. h6",
+            "D. heading"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which is used to create Web Pages ?",
+        answer: "C. HTML",
+        options: [
+            " A. C++",
+            "B. Java",
+            "C. HTML",
+            "D. JVM"
+        ]
+    },
+    {
+        numb: 7,
+        question: "HTML is a set of markup ___.",
+        answer: "A. tags",
+        options: [
+            "A. tags",
+            "B. sets",
+            "C. attributes",
+            "D. none of the above"
+        ]
+    },
+    {
+        numb: 8,
+        question: "HTML tags are used to describe document ___.",
+        answer: "B. content",
+        options: [
+            " A. definition",
+            "B. content",
+            "C. language",
+            "D. model"
+        ]
+    },
+    {
+        numb: 9,
+        question: "HTML program is saved using ___ extension.",
+        answer: "B. .html",
+        options: [
+            " A. .htmn",
+            "B. .html",
+            "C. .htnl",
+            "D. .htnl"
+        ]
+    },
+    {
+        numb: 10,
+        question: "HTML program can be read and rendered by ___.",
+        answer: "C. Web Browser",
+        options: [
+            " A. Compiler",
+            "B. Server",
+            "C. Web Browser",
+            "D. Interpreter"
+        ]
+    }
+]
